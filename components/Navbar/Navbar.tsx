4a5b55f74476a63a3ea80f0290c80ae594ea5917
Tyler Renfro <v.renfro6@gmail.com>
2025-08-04 18:03:09 -0600

    adds test content for home page

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -8,26 +8,32 @@ function Navbar() {
         async function fetchNavigation() {
             const getNav = await fetch("https://cdn.builder.io/api/v3/content/navigation?apiKey=49da8b8581a648f6989d85ec423cf285");
             const data = await getNav.json();
-            setnavData(data.results);  
-            console.log(data.results);          
+            setnavData(data.results);
+            console.log(data.results);
         };
 
         fetchNavigation();
 
     }, []);
     return (
-        
-        <div>
+        <>
             {navData.length > 0 && (
-                <div>
-                    {navData.map((item: any, index: number) => (
-                        <div key={index}>
-                            <a href={item.data.path}>{item.data.name}</a>                            
-                        </div>
-                    ))}
+            <div className="navbar bg-base-100 shadow-sm" style={{ marginTop: 0 }}>
+                <div className="flex-1">
+                    <a className="btn btn-ghost text-xl">daisyUI</a>
                 </div>
-            )}        
-        </div>
+                <div className="flex-none">
+                    <ul className="menu menu-horizontal px-1">
+                        {navData.map((item: any, index: number) => (
+                            <li key={index}>
+                                <a href={item.data.path}>{item.data.name}</a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        )}
+        </>
     );
 }
 
